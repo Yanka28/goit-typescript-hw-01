b706a68e3b0d162a7c5c1afa0eafcffb939caa84
Yanka28 <yanachepeluk@ukr.net>
2023-10-25 23:54:02 +0300

    com4

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import { concatenation } from './concatenation';
+import { concatenation } from '../dist/concatenation';
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 if (button && input) {
